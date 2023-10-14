c92ec24f0874432d6be6aadf4fc0e148ef9a9b78
Ola <olo2040@wp.pl>
2023-10-14 10:50:52 +0200

    finished nav section

--- js/main.js
+++ js/main.js
@@ -7,8 +7,19 @@ const handleNav = () => {
 
 	allNavItems.forEach((item) => {
 		item.addEventListener("click", () => {
-			nav.classList.remove("nav--active" );
+			nav.classList.remove("nav--active");
 		});
+	});
+	handleNavItemsAnimation();
+};
+
+const handleNavItemsAnimation = () => {
+	let delayTime = 0;
+
+	allNavItems.forEach(item => {
+		item.classList.toggle("nav-items-animation");
+		item.style.animationDelay = "." + delayTime + "s";
+		delayTime++;
 	});
 };
 
